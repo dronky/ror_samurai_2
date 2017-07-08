77ebda55b3f4e720829dae67faa3aec35194f37e
Andrey <vkontaktenoob01@mail.ru>
2017-07-08 06:32:58 -0700

    new app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :trains
   resources :railway_stations
+  resources :routes
 
-  root 'trains#index'
+  # root 'trains#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703210317) do
+ActiveRecord::Schema.define(version: 20170708122113) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
