b4a025d2e7df27aab61a2f18f338321fb88db22f
Andrey Makarenko <photobitmiitsupp@gmail.com>
2017-08-04 20:28:08 +0300

    l15 fix2

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -2,12 +2,4 @@ class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
   validates :station_number, uniqueness: {scope: :route_id}
-  before_validation :set_number
-
-  private
-
-  def set_number
-    # self.where(ids.last).update(station_number: self.pluck(:station_number).max + 1)
-    puts '123'
-  end
 end
--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -3,5 +3,13 @@ class Wagon < ApplicationRecord
   belongs_to :wagon_type
 
   validates :train_id, :wagon_type_id, :up_seats, :down_seats, presence: true
+
+  after_commit :set_wagon_number
+
+  private
+
+  def set_wagon_number
+
+  end
 end
 
