44133be526c1bd244a93f954750ef99bd8a59a06
Andrey Makarenko <dronskiy95@yandex.ru>
2017-08-05 14:55:25 +0300

    bool train

--- app/models/train.rb
+++ app/models/train.rb
@@ -9,32 +9,10 @@ class Train < ApplicationRecord
     compartment_count = wagons.joins(:wagon_type).where('wagon_types.title': :compartment_car).count
     seats = {second: second_class_count, comp: compartment_count}
   end
+
+  #ne rabotaet :(
+  def select_seats(wagon_type, seats)
+    self.wagons.where(type: wagon_type).seats
+  end
+
 end
-
-
-# attr_accessor :seats
-#
-# #code will be useful for setting the seats for wagons in the admin panel
-# @all_up_seats = 10
-# @all_down_seats = 10
-# @seats = {up: 0, down: 0}
-#
-# def total_seats
-#   total = 0
-#   self.wagons.each do |wagon|
-#     total+=wagon.up_seats
-#     total+=wagon.down_seats
-#   end
-#   total
-# end
-#
-# def set_seat(up_seats_count = 0, down_seats_count = 0)
-#   raise "Up seats should more than 0" if @all_up_seats <= 0
-#   raise "Down seats should more than 0" if @all_down_seats <= 0
-#   if up_seats_count && down_seats_count
-#     @seats[up] = up_seats_count
-#     @all_up_seats -= up_seats_count
-#     @seats[down] = down_seats_count
-#     @all_down_seats_seats -= down_seats_count
-#   end
-# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804125002) do
+ActiveRecord::Schema.define(version: 20170805110849) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170804125002) do
     t.datetime "updated_at", null: false
     t.integer "route_id"
     t.integer "current_station_id"
+    t.boolean "head"
     t.index ["current_station_id"], name: "index_trains_on_current_station_id"
   end
 
