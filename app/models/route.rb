1539a60f4ffb5e117e0338480fdf42367dd114c3
Andrey <vkontaktenoob01@mail.ru>
2017-08-11 13:28:03 -0700

    routes trouble

--- app/models/route.rb
+++ app/models/route.rb
@@ -16,8 +16,14 @@ class Route < ActiveRecord::Base
   def self.search(first_station, last_station)
     if first_station && last_station
       p first_station
-      # RailwayStationsRoute.select(:route_id).where("(railway_station_id = #{first_station} or railway_station_id = #{last_station})").first.route_id
-      @route = Route.find(2)
+
+      RailwayStationsRoute.select(:route_id).where("railway_station_id = #{first_station} or railway_station_id = #{last_station}")
+      #вверху мы взяли роут, в который входит 1 из станций. Надо проверить его на вхождение другой станции еще в 1 запросе
+
+
+
+      #RailwayStationsRoute.select(:route_id).where("(railway_station_id = #{first_station} and railway_station_id = #{last_station})").first.route_id
+      #@route = Route.find(2)
     else
       all
     end
