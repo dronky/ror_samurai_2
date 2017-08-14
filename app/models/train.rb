43c6b2f0ca934b9ce5c21431d04ccf1d683cdbd6
Andrey <vkontaktenoob01@mail.ru>
2017-08-14 11:55:52 -0700

    trains_create_hotfix

--- app/models/train.rb
+++ app/models/train.rb
@@ -10,6 +10,11 @@ class Train < ApplicationRecord
     seats = {second: second_class_count, comp: compartment_count}
   end
 
+  def set_station
+    self.current_station = (RailwayStation.joins(:railway_stations_routes).where("route_id = ?", self.route_id) &&
+        RailwayStation.joins(:railway_stations_routes).where("station_number = 1")).first
+  end
+
   def select_seats(wagon_type, seats_type)
     self.wagons.where(wagon_type: wagon_type).sum(seats_type.to_sym)
   end
