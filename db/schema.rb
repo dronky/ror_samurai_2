d663f222f352a759ac6472575f30bf0ae2fd5afe
Andrey Makarenko <dronskiy95@yandex.ru>
2017-08-29 21:09:18 +0300

    task7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828143606) do
+ActiveRecord::Schema.define(version: 20170829180758) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170828143606) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.boolean "admin", default: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
