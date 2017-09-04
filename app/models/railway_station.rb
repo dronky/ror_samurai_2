d6be646cf670e0a515150686db5426fe7acbec0e
Andrey Makarenko <photobitmiitsupp@gmail.com>
2017-09-04 19:04:17 +0300

    l19 1, 2

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -3,6 +3,8 @@ class RailwayStation < ApplicationRecord
   has_many :trains, class_name:'Train', foreign_key: :current_station_id
   has_many :railway_stations_routes
 
+  validates :title, presence: true
+
   scope :ordered, -> { select('railway_stations.*, railway_stations_routes.station_number').joins(:railway_stations_routes).order("railway_stations_routes.station_number").uniq }
 
   def update_position(route, position)
