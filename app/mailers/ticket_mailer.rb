1a24a262488ef233c7ccfa0a794cc52f929fda80
Andrey Makarenko <dronskiy95@yandex.ru>
2017-09-09 00:52:45 +0300

    l20 2 progress

---
+++ app/mailers/ticket_mailer.rb
@@ -0,0 +1,7 @@
+class TicketMailer < ApplicationMailer
+  def buy_ticket(user,ticket)
+    @user = user
+    @ticket = ticket
+    mail(to: user.email, subject: 'Вы купили билет')
+  end
+end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,7 +5,16 @@ class Ticket < ApplicationRecord
   belongs_to :end_station, class_name: 'RailwayStation'
   before_save :set_user_name, on: :create
 
+  after_create :send_new_ticket
+  def route_name
+    "#{start_station.title} - #{end_station.title}"
+  end
+
   def set_user_name
     self.full_name =  self.user.name
+  end
+
+  def send_new_ticket
+    TicketMailer.buy_ticket(self.user,self).deliver_now
   end
 end
