cd1d9f8f94076908d070a504bb0acf23e67861e7
dronky <dronskiy95@yandex.ru>
2017-09-11 23:00:16 +0300

    trains fix

--- app/controllers/admin/trains_controller.rb
+++ app/controllers/admin/trains_controller.rb
@@ -39,7 +39,7 @@ class Admin::TrainsController < Admin::BaseController
   def update
     respond_to do |format|
       if @train.update(train_params)
-        format.html { redirect_to @train, notice: 'Train was successfully updated.' }
+        format.html { redirect_to [:admin,@train], notice: 'Train was successfully updated.' }
       else
         format.html { render :edit }
       end
